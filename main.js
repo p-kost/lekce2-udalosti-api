28c3a881af74cd6dc59c4761a4c750c155ddad5b
p-kost <petra.kostal1@gmail.com>
2022-03-05 15:50:47 +0100

    přidání funkcí k tlčítkům měnícím text

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-// tady je místo pro náš program
 
-console.log('ahoj');
+function getFat(odstavecID) {
+    let odstavec = document.getElementById(odstavecID);
+    odstavec.classList.toggle('bold-text');
+}
+
+function turnRed(odstavecID) {
+    let odstavec = document.getElementById(odstavecID);
+    odstavec.classList.toggle('red-text');
+}
+
+
+
+// Zvětšení mi bohužel nefunguje, pořád mi to hlásí, že fontsize není číslo
+
+/** 
+ *    @param {int} soucasnaVelikost
+ *    @param {int} zmenaVelikosti
+ * 
+*/
+
+function getBigger(textSelector, zmenaVelikosti) {
+    let text = document.querySelector(textSelector);
+    let soucasnaVelikost = parseInt(text.style.fontSize);
+    text.style.fontSize = (soucasnaVelikost + zmenaVelikosti) + 'px';
+}
+
